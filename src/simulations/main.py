6543f6be13d64838248f107211368f8aca2c4411
Mojahed Jaber <129369338+Mojahed91@users.noreply.github.com>
2023-08-16 16:23:40 +0300

    Update main.py

--- src/simulations/main.py
+++ src/simulations/main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     n_qubits = 2
     kik_obj = KikCalculation(n_qubits, InitialState(n).generate_excited_state(), QuantumFourierTransform(n))
 
-    # kik_obj.plot_controllable_and_uncontrollable_coh_vs_pauli_and_native_errors()
-    kik_obj.plot_pauli_and_native_vs_coh_errors()
+    kik_obj.plot_controllable_and_uncontrollable_coh_vs_pauli_and_native_errors()
+    # kik_obj.plot_pauli_and_native_vs_coh_errors()
