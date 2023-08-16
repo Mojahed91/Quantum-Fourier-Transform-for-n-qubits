1e2ffa401996bd8da8e95c1f3710913a08845f82
Mojahed Jaber <129369338+Mojahed91@users.noreply.github.com>
2023-08-16 15:16:46 +0300

    Update cXGate.py

--- src/simulations/cXGate.py
+++ src/simulations/cXGate.py
@@ -7,36 +7,6 @@ from quantumGate import QuantumGate
 from qiskit import QuantumRegister, QuantumCircuit
 
 
-class IDGate(QuantumGate):
-    def __init__(self, n_qubits):
-        super().__init__(n_qubits)
-
-    def get_matrix(self):
-        return Utilities.single_q_gate_for_n_q(0, self.n, Utilities.I)
-
-    def run_pauli_combinations(self, oper):
-        """Compute the inverse operation in Liouville space for a given number 'n' of qubits."""
-        init = Utilities.recursive_kron([I] * self.n)
-        dressed_oper = np.dot(np.kron(init, init), 0)
-
-        def generate_pauli_combinations(depth, indices):
-            nonlocal dressed_oper
-            if depth == self.n:
-                combined_pauli = Utilities.recursive_kron([Utilities.pauli[i] for i in indices])
-                sig_b_L = Utilities.make_liouville(combined_pauli)
-                dressed_oper = dressed_oper + np.dot(np.dot(sig_b_L, oper), sig_b_L)
-                return
-            for i in range(4):
-                generate_pauli_combinations(depth + 1, indices + [i])
-
-        generate_pauli_combinations(0, [])
-        return np.dot(dressed_oper, 1 / (4 ** self.n))
-
-    def dress_id_operator_by_rc_gates(self, oper, n):
-        """Dress up the Identity gate with Randomised Compiling gates."""
-        return self.run_pauli_combinations(oper, n)
-
-
 class CXGate(QuantumGate):
     NUM_RC_CX = 16
     LEFT_RC = 0
