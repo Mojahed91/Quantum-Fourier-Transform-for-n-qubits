1349b56d567624f97a22bf365b5f828217f87071
Mojahed Jaber <129369338+Mojahed91@users.noreply.github.com>
2023-08-20 13:53:01 +0300

    Update iDGate.py

--- src/simulations/iDGate.py
+++ src/simulations/iDGate.py
@@ -5,13 +5,33 @@ from utilities import Utilities
 
 class IDGate(QuantumGate):
     def __init__(self, n_qubits):
+        """
+        Initializes the IDGate class with a given number of qubits.
+
+        Args:
+            n_qubits (int): The number of qubits.
+        """
         super().__init__(n_qubits)
 
     def get_matrix(self):
+        """
+        Gets the matrix representation of the identity gate for a system of n qubits.
+
+        Returns:
+            np.ndarray: The matrix representation of the identity gate.
+        """
         return Utilities.single_q_gate_for_n_q(0, self.n, Utilities.I)
 
     def run_pauli_combinations(self, oper):
-        """Compute the inverse operation in Liouville space for a given number 'n' of qubits."""
+        """
+        Compute the inverse operation in Liouville space for a given number 'n' of qubits.
+
+        Args:
+            oper (np.ndarray): The quantum operator to compute the inverse operation on.
+
+        Returns:
+            np.ndarray: The inverse operation in Liouville space.
+        """
         init = Utilities.recursive_kron([Utilities.I] * self.n)
         dressed_oper = np.dot(np.kron(init, init), 0)
 
@@ -29,5 +49,15 @@ class IDGate(QuantumGate):
         return np.dot(dressed_oper, 1 / (4 ** self.n))
 
     def dress_id_operator_by_rc_gates(self, oper, n):
+        """
+        Dress up the Identity gate with Randomised Compiling gates.
+
+        Args:
+            oper (np.ndarray): The quantum operator to dress.
+            n (int): The number of qubits.
+
+        Returns:
+            np.ndarray: The dressed operator.
+        """
         """Dress up the Identity gate with Randomised Compiling gates."""
         return self.run_pauli_combinations(oper, n)
