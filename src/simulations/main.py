d19a26fafded81c3fa9ef2823ebc2bb16752cfbd
Mojahed Jaber <129369338+Mojahed91@users.noreply.github.com>
2023-09-04 07:00:19 +0300

    Update main.py

--- src/simulations/main.py
+++ src/simulations/main.py
@@ -3,8 +3,19 @@ from initialState import InitialState
 from quantumFourierTransform import QuantumFourierTransform
 
 if __name__ == '__main__':
+    # Define the number of qubits to be used in the calculations
     n_qubits = 2
-    kik_obj = KikCalculation(n_qubits, InitialState(n).generate_excited_state(), QuantumFourierTransform(n))
+    
+    # Instantiate the KikCalculation class:
+    # 1. Specify the number of qubits.
+    # 2. Generate the initial excited state.
+    # 3. Apply the Quantum Fourier Transform.
+    kik_obj = KikCalculation(n_qubits, InitialState(n_qubits).generate_excited_state(), QuantumFourierTransform(n_qubits))
 
+    # Plot the comparison between controllable and uncontrollable coherences 
+    # versus the effects of Pauli and native errors.
     kik_obj.plot_controllable_and_uncontrollable_coh_vs_pauli_and_native_errors()
+
+    # Uncomment the next line to plot the comparison of Pauli and native errors 
+    # against coherence errors.
     # kik_obj.plot_pauli_and_native_vs_coh_errors()
