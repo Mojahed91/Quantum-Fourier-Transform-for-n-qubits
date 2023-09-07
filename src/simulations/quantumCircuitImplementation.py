a1b2abdbe37363f6dee6b8a990d15aca552f291a
Mojahed Jaber <129369338+Mojahed91@users.noreply.github.com>
2023-09-07 20:09:09 +0300

    Update quantumCircuitImplementation.py

--- src/simulations/quantumCircuitImplementation.py
+++ src/simulations/quantumCircuitImplementation.py
@@ -1,39 +1,84 @@
-
-
 class QuantumCircuitImplementation:
     """
     Base class for Quantum circuit operations.
+    
+    This class is designed to serve as an abstract layer for quantum circuit operations.
+    Concrete implementations should be provided by subclasses.
+    
     Shared quantum methods or attributes should be placed here.
     """
 
     def __init__(self, n_qubits):
+        """
+        Initializes the QuantumCircuitImplementation instance.
+
+        Args:
+            n_qubits (int): The number of qubits for the circuit.
+
+        """
         self.n_qubits = n_qubits
+        # Initialize error attributes to None by default.
+        self.avg_two_qubit_error = None
+        self.avg_one_qubit_error = None
 
     def forward_circuit(self):
+        """
+        Abstract method for the forward circuit operation.
+
+        """
         raise NotImplementedError("Forward circuit method not implemented.")
 
     def backward_circuit(self):
+        """
+        Abstract method for the backward circuit operation.
+
+        """
         raise NotImplementedError("Backward circuit method not implemented.")
 
     def forward_circuit_with_rc(self):
+        """
+        Abstract method for the forward circuit operation with RC.
+
+        It's unclear what "with_rc" signifies. Consider adding more descriptive docstrings or renaming the method for clarity.
+
+        """
         raise NotImplementedError("Forward circuit with RC method not implemented.")
 
     def backward_circuit_with_rc(self):
+        """
+        Abstract method for the backward circuit operation with RC.
+
+        """
         raise NotImplementedError("Backward circuit with RC method not implemented.")
 
     @property
     def two_qubit_error(self):
+        """Property to get the average two qubit error."""
         return self.avg_two_qubit_error
 
     @two_qubit_error.setter
     def two_qubit_error(self, avg_2q_error):
+        """
+        Setter for the average two qubit error.
+
+        Args:
+            avg_2q_error (float): The average two qubit error to set.
+
+        """
         self.avg_two_qubit_error = avg_2q_error
 
     @property
     def one_qubit_error(self):
+        """Property to get the average one qubit error."""
         return self.avg_one_qubit_error
 
     @one_qubit_error.setter
     def one_qubit_error(self, avg_1q_error):
-        self.avg_one_qubit_error = avg_1q_error
+        """
+        Setter for the average one qubit error.
 
+        Args:
+            avg_1q_error (float): The average one qubit error to set.
+
+        """
+        self.avg_one_qubit_error = avg_1q_error
